22108671629bb417a4f7da1a55dbac39c874be35
Jonathan Brink <Jonathan.Brink@sas.com>
2014-09-05 23:31:03 -0400

    initial

---
+++ action.js
@@ -0,0 +1,44 @@
+/*
+function pics() {
+   var aPics = [
+       '01.jpg',
+       '02.jpg',
+       '03.jpg',
+       '04.jpg',
+       '05.jpg',
+       '06.jpg',
+       '07.jpg',
+       '08.jpg',
+       'invitationPhoto.jpg'
+   ];
+
+   for( var i = 0; i < aPics.length; i++ ) {
+       $('#photossection .lazypics').append('<a href="pics/original/pre/' + aPics[i] + '"><img src="pics/resized/pre/' + aPics[i] + '" /></a>');
+   }
+
+   var aPics_wedding = [
+       'brinkPhoto4.JPG',
+       'daveAndMom.JPG',
+       'daveAndTracy.JPG',
+       'exchangingRings2.JPG',
+       'exitingHouse.JPG',
+       'groupPhoto5.JPG',
+       'inFrontOfCourthouse.JPG',
+       'momAndJacobBowling2.JPG',
+       'parents2.JPG',
+       'postWeddingDinnerGroupPhoto1.JPG',
+       'postWeddingGroupPhoto.JPG',
+       'tracyTakingSelfieWithJacob.jpg'
+   ];
+   for( var i = 0; i < aPics.length; i++ ) {
+       $('#photossection_wedding .lazypics').append('<a href="pics/original/post/' + aPics_wedding[i] + '"><img src="pics/resized/post/' + aPics_wedding[i] + '" /></a>');
+   }
+}
+*/
+
+$(document).ready(function() {
+    setTimeout(function() {
+        //pics();
+    }, 1000);
+});
+
